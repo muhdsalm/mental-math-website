5c0b1c0deaee512e9a64c91fc6274090b0ad7d18
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-07 13:38:10 +0000

    potentially fix an issue with the html shell

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725715886|3250820821';
+const CACHE_VERSION = '1725716251|3615765684';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
