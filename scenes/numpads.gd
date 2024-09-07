b4693825497f85add27fa1ba9e76e95f08ee9295
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-07 13:33:28 +0000

    Fix number pad in android landscape not working

--- scenes/numpads.gd
+++ scenes/numpads.gd
@@ -11,11 +11,18 @@ func _process(delta: float) -> void:
 	pass
 
 func type_in_question_linedit(number: String):
-	var QuestionLineEdit: LineEdit = get_node("%QuestionLineEdit2")
+	var QuestionLineEdit: LineEdit
+	if get_viewport_rect().size.y > get_viewport_rect().size.x:
+		QuestionLineEdit = get_node("%QuestionLineEdit2")
+	else:
+		QuestionLineEdit = get_node("%QuestionLineEdit")
 	if number == "backspace":
 		QuestionLineEdit.text = QuestionLineEdit.text.substr(0, len(QuestionLineEdit.text) - 1)
 	else:
 		QuestionLineEdit.text += number
 		
 func parent_line_changed():
-	get_tree().current_scene.line_edit_changed_function(get_node("%QuestionLineEdit2").text)
+	if get_viewport_rect().size.y > get_viewport_rect().size.x:
+		get_tree().current_scene.line_edit_changed_function(get_node("%QuestionLineEdit2").text)
+	else:
+		get_tree().current_scene.line_edit_changed_function(get_node("%QuestionLineEdit").text)
