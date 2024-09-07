aeca36d9dccb01783c651e0e5fc8a30ccd645b22
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-07 13:47:47 +0000

    Fix the loading screen by using a custom HTML shell

--- scenes/numpads.gd
+++ scenes/numpads.gd
@@ -20,7 +20,7 @@ func type_in_question_linedit(number: String):
 		QuestionLineEdit.text = QuestionLineEdit.text.substr(0, len(QuestionLineEdit.text) - 1)
 	else:
 		QuestionLineEdit.text += number
-		
+			
 func parent_line_changed():
 	if get_viewport_rect().size.y > get_viewport_rect().size.x:
 		get_tree().current_scene.line_edit_changed_function(get_node("%QuestionLineEdit2").text)
