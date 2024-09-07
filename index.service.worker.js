8940b3ad0d50a443b18b316fd7fdfa3ba4699d1e
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-07 13:41:18 +0000

    I lose, alright Godot? I'll put your stupid GODOT_CONFIG back without the key that makes the issue. Happy now?

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725716251|3615765684';
+const CACHE_VERSION = '1725716418|3782230502';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
