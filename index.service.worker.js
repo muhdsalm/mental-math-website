286758189f72d4813029166cf2aaf6d6a2bccaa7
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-08 10:18:54 +0000

    add proper question marker

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725716897|4261519429';
+const CACHE_VERSION = '1725790708|6794949413';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
