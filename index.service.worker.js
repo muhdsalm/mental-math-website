b2f247f4c6d57a690295073aa40da8650ee8b055
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-08 17:06:31 +0000

    actually fix the problem now maybe

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725814997|18825592824';
+const CACHE_VERSION = '1725815174|19002099967';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
