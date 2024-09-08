4de7ba298148d5bf00cf1ba8a1ecb64f3e0a119c
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-08 10:39:52 +0000

    Shrink the Mental Math text in the loading screen

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725791821|7906064494';
+const CACHE_VERSION = '1725791973|8058391971';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
