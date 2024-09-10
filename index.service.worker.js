23509d93d72745904c6e2f709e151e7b0e361025
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-10 10:46:22 +0000

    Shift difficulty buttons to start page

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725815174|19002099967';
+const CACHE_VERSION = '1725965135|2307411115';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
