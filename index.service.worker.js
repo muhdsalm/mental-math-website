f6e5aa2dead8a2c8a73803320d9f780229468761
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-10 18:21:07 +0000

    Add difficulty to the globals

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725990231|4497067626';
+const CACHE_VERSION = '1725992452|6051619187';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
