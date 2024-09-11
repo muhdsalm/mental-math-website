872620725740e21221184741de3092f0b344078c
Muhammad Salman <muhdsalm44@gmail.com>
2024-09-11 16:22:00 +0000

    Add return button

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1725992452|6051619187';
+const CACHE_VERSION = '1726071700|990106852';
 /** @type {string} */
 const CACHE_PREFIX = 'Mental Math Webs-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
